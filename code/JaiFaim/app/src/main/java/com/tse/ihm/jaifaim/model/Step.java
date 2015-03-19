c68d87998bfca14cf0c814ed9a622a546a2b93e7
GWillmann <gregwillm@gmail.com>
2015-03-19 22:21:14 +0100

    Ajout du serializedname dans la classe step

--- code/JaiFaim/app/src/main/java/com/tse/ihm/jaifaim/model/Step.java
+++ code/JaiFaim/app/src/main/java/com/tse/ihm/jaifaim/model/Step.java
@@ -1,10 +1,13 @@
 package com.tse.ihm.jaifaim.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Description : Modele d'une étape pour la création d'une recette
  * Created by Gabriel on 14/03/15.
  */
 public class Step {
+    @SerializedName("description")
     private String m_Description;
 
     public Step(String _description) {
